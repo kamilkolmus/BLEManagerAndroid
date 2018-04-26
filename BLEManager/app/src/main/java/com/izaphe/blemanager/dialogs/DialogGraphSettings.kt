228a32298453f346e7f8b2cea717818f208e3e11
Kamil Kolmus <kamil.kolmus@gmail.com>
2018-04-26 02:51:37 +0200

    add preferences for graph

--- BLEManager/app/src/main/java/com/izaphe/blemanager/dialogs/DialogGraphSettings.kt
+++ BLEManager/app/src/main/java/com/izaphe/blemanager/dialogs/DialogGraphSettings.kt
@@ -1,54 +1,52 @@
 package com.izaphe.blemanager.dialogs
 
+
 import android.app.Dialog
 import android.content.Context
 import android.os.Bundle
-import android.text.InputFilter
+import android.support.v7.app.AppCompatActivity
+import android.view.InflateException
 import android.view.Window
-import kotlinx.android.synthetic.main.dialog_graph_settings.*
-import android.widget.Toast
 import com.i7xaphe.blemanager.R
 
 
-import com.izaphe.blemanager.filters.InputFilterMinMax
-import com.izaphe.blemanager.myinterfaces.DialogGraphSettingsInterface
+
+class DialogGraphSettings(context: Context?,activity: AppCompatActivity) : Dialog(context) {
 
 
-class DialogGraphSettings(context: Context?, dialogGraphSettingsInterface: DialogGraphSettingsInterface, historySize:Int) : Dialog(context) {
-
-    var dialogGraphSettingsInterface =dialogGraphSettingsInterface
-    var historySize:Int=historySize
+    val activity=activity
+    var removeFragment=true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(R.layout.dialog_graph_settings)
-
-        et_historysize.filters = arrayOf<InputFilter>(InputFilterMinMax(1, 1000))
-
-        et_historysize.setText(historySize.toString())
-
-        b_save_graph_settings.setOnClickListener({
-            try {
-                historySize = Integer.parseInt(et_historysize.text.toString())
-                if(historySize<10){
-                    historySize=10
-                    Toast.makeText(context, "Min history size is 10", Toast.LENGTH_SHORT).show()
-                }
-                dialogGraphSettingsInterface.historySize(historySize)
-                dismiss()
-                cancel()
-            } catch (e: NumberFormatException) {
-                Toast.makeText(context, "NumberFormatException: history size", Toast.LENGTH_SHORT).show()
-            }
-
-        })
-
-        b_cancel_graph_dialog.setOnClickListener({
-            dismiss()
-            cancel()
-        })
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+       // setTitle("Settings")
     }
 
+    override fun onAttachedToWindow() {
+        super.onAttachedToWindow()
+        try {
+            setContentView(R.layout.dialog_graph_settings_fragment)
+        }catch (e: InflateException){
+            e.printStackTrace()
+            cancel()
+            removeFragment=false
+        }
+    }
+
+    override fun onDetachedFromWindow() {
+        super.onDetachedFromWindow()
+        if(removeFragment){
+            val fm = activity.supportFragmentManager
+            val xmlFragment = fm.findFragmentById(R.id.fragment_graph_settings)
+            if (xmlFragment != null) {
+                fm.beginTransaction().remove(xmlFragment).commit()
+            }
+        }
+
+    }
+    companion object {
+        val TAG:String=DialogGraphSettings::class.java.simpleName
+    }
 
 }
